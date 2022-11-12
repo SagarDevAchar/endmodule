28adcb001fd2a7debda2be2f7a87920853b93fa0
Sagar Dev Achar <51400137+SagarDevAchar@users.noreply.github.com>
2022-11-12 14:41:54 +0530

    Update rca.v

--- n-Bit Ripple Carry Adder/rca.v
+++ n-Bit Ripple Carry Adder/rca.v
@@ -1,18 +1,18 @@
 module rca #(parameter width = 64) (
-    output C,
-    output [width-1:0] S,
+    output CARRY,
+    output [width-1:0] SUM,
     input [width-1:0] a, b,
     input c
     );
     
-    wire [width:0] CARRY;
+    wire [width:0] C;
     
-    assign C = CARRY[width];
-    assign CARRY[0] = c;
+    assign CARRY = C[width];
+    assign C[0] = c;
     
     genvar i;
     generate for (i = 0 ; i < width ; i = i + 1) begin: rca_fa
-        fa u (CARRY[i+1], S[i], a[i], b[i], CARRY[i]);
+        fa u (CARRY[i+1], SUM[i], a[i], b[i], CARRY[i]);
     end
     endgenerate
     
