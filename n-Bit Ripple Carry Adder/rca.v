8bd03f5c6c1e6edde2e32aa1d1edc925cb5a9b23
Sagar Dev Achar <51400137+SagarDevAchar@users.noreply.github.com>
2022-11-12 13:57:22 +0530

    Create rca.v

---
+++ n-Bit Ripple Carry Adder/rca.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+module rca #(parameter width = 64) (
+    output C,
+    output [width-1:0] S,
+    input [width-1:0] a, b,
+    input c
+    );
+    
+    wire [width:0] CARRY;
+    
+    assign C = CARRY[width];
+    assign CARRY[0] = c;
+    
+    genvar i;
+    generate for (i = 0 ; i < width ; i = i + 1) begin: rca_fa
+        fa u (CARRY[i+1], S[i], a[i], b[i], CARRY[i]);
+    end
+    endgenerate
+    
+endmodule
